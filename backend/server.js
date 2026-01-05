f5edd0a6dd825dcf0ae0f638c99491c41421866d
oumayma14 <oumaima0fazani@gmail.com>
2026-01-06 00:19:38 +0100

    Server configuration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
+dotenv.config();    
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    Credentials: true
+}))
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app;
 
 const server = async () => {
-    console.log("Server is running");
+    try {
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (error) {
+        console.log("Failed to start server ...",error.message);
+        process.exit(1);
+    }
 };
 server();
