d3336a0eacb86409402d72de7ea097c0f851369a
Oumayma <oumaima0fazani@gmail.com>
2026-01-14 10:24:58 +0100

    nothing changed

--- backend/src/controllers/auth/UserController.js
+++ backend/src/controllers/auth/UserController.js
@@ -20,7 +20,7 @@ export const registerUser = asyncHandler(async (req, res) => {
     //check user existence
     const userExists = await User.findOne({ email });
     if (userExists) {
-        return res.status(400).json({ message: "User already exists" });
+        return res.status(400).json({ message: "User already exists..." });
     }
 
     //create user
